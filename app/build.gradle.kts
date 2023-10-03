8f844984b168ef679edbf88d84d79ba82e513fb1
chhue <chhue96@gmail.com>
2023-10-03 17:15:08 +0900

    initgit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
@@ -44,4 +50,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    // other dependencies
+    // Compose
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
